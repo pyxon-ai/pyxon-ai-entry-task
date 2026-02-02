3b6cb7fbd70cbd9ee8add65397d6f763ddf8b754
EyadAlN3imi <eyadnaimi71@gmail.com>
2026-02-02 06:55:17 +0300

    BACK TO THE SRC CODE

--- src/main.py
+++ src/main.py
@@ -2,10 +2,8 @@ from fastapi import FastAPI
 from routes.base import base_router
 from routes.chunk import chunk_router
 from routes.search import search_router
-from routes.evaluate import evaluate_router
 
 app = FastAPI()
 app.include_router(base_router)
 app.include_router(chunk_router)
 app.include_router(search_router)
-app.include_router(evaluate_router)
